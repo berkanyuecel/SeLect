45b34931e3351ce8bde08b8bf2a907bd846abf0c
Berkan Yuecel <berkan.yuecel@adp.com>
2023-10-11 15:46:13 +0200

    git init

---
+++ src/main/java/byuecel/labor/SeLect/SeLectApplication.java
@@ -0,0 +1,13 @@
+package byuecel.labor.SeLect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeLectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeLectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/byuecel/labor/SeLect/SeLectApplicationTests.java
@@ -0,0 +1,13 @@
+package byuecel.labor.SeLect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeLectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
